c1f6217cb9450bac1bc70932c00e113f04de455f
RobyLedda94 <robyledda01@yahoo.it>
2024-09-06 16:11:57 +0200

    [UPD] struttura layout no logica

--- index.php
+++ index.php
@@ -9,9 +9,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- cdn bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Password</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Genera Password</h1>
+                <form method="GET" action="index.php">
+                    <div class="mb-3">
+                        <label for="lunghezza" class="form-label"><strong>Lunghezza della password :</strong></label>
+                        <input type="text" class="form-control" name="lunghezza" id="lunghezza">
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">Genera Password</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
