294346db1371516881a75a66437cf7df32e6c73b
RobyLedda94 <robyledda01@yahoo.it>
2024-09-06 18:27:24 +0200

    [UPD] sposto la funzione nel fule function php e lo includo nel file index

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
 <?php 
-// definisco la funzione per generare una password casuale
-function generaPassword($lunghezza) {
-    // variabile che contiene i caratteri speciali
-    $caratteri = 'abcdefghilmnopqrstuvwxyzABCDEFGHILMNOPQRSTUVWXYZ0123456789!@#%$';
-    // variabile che salva la password generata
-    $password = '';
 
-    // ciclo per generare la password
-    for ($i = 0; $i < $lunghezza; $i++) {
-        $carattere = $caratteri[rand(0, strlen($caratteri) -1 )];
-        $password .= $carattere;
-    }
-
-    return $password;
-    
-}
+// includo il file function
+include __DIR__ . '/function.php';
 
 // variabile di controllo ed assegnazzione della lunghezza 
 $lunghezza = isset($_GET['lunghezza']) && is_numeric($_GET['lunghezza']) && $_GET['lunghezza'] > 0 ? intval($_GET['lunghezza']) : 10;
